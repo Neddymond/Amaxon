a0fc1c67d96b4f873c22870fdb190008a8395e90
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2021-01-28 06:39:02 +0100

    Converted the static web app to a react app

---
+++ README.md
@@ -0,0 +1,2 @@
+Amaxon is an Amazon clone created using Node.JS and React.
+This project waas inspired by @basir
---
+++ frontend/src/App.js
@@ -0,0 +1,130 @@
+function App() {
+  return (
+    <div className="grid-container">
+      <header className="row">
+        <div>
+          <a className="brand" href="index.html">amaxon</a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+      <main>
+        <div className="row center">
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./Images/p1.jpg" alt="product"/>
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Sony WH-1000XM4</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">$120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./Images/p1.jpg" alt="product"/>
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Sony WH-1000XM4</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">$120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./Images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Sony WH-1000XM4</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">$120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./Images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Sony WH-1000XM4</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">$120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./Images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Sony WH-1000XM4</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">$120</div>
+            </div>
+          </div>
+          <div className="card">
+            <a href="product.html">
+              <img className="medium" src="./Images/p1.jpg" alt="product" />
+            </a>
+            <div className="card-body">
+              <a href="product.html">
+                <h2>Sony WH-1000XM4</h2>
+              </a>
+              <div className="rating">
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+                <span><i className="fa fa-star"></i></span>
+              </div>
+              <div className="price">$120</div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <footer className="row center">All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;

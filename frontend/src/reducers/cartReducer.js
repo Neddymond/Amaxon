39a12e8c8779c2de902d390325457b3c6bbe16ed
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2021-02-26 11:41:52 +0100

    Creates function to list purchased items in the cart, together with their quantity and price.

--- frontend/src/reducers/cartReducer.js
+++ frontend/src/reducers/cartReducer.js
@@ -1,27 +1,27 @@
 import { CART_ADD_ITEM } from "../constants/cartConstant";
 
 export const cartReducer = (state = { cartItems: [] }, action) => {
-  console.log(state.cartItems.length);
+  // console.log(state.cartItems.length);
   switch(action.type) {
     case CART_ADD_ITEM:
       const item = action.payload;
       const existItem = state.cartItems.find((x) => {
-        console.log(x.productId);
-        console.log(item.productId);
+        // console.log(x.productId);
+        // console.log(item.productId);
         return x.productId === item.productId
-
       });
+      // console.log(existItem)
       if (existItem) {
-        console.log("yes");
+        // console.log("yes");
         return {
           ...state,
-          cartItems: state.cartItems.map((x) => x.product === existItem.product
+          cartItems: state.cartItems.map((x) => x.productId === existItem.productId
             ? item
             : x
           )
         }
       } else {
-        console.log("no");
+        // console.log("no");
         return {
           ...state,
           cartItems: [...state.cartItems, item]

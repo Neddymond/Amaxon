d76dff45c09246dc628ebedcaf6fbd06c5bce97e
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2021-03-08 21:17:10 +0100

    Creates a shipping address screen to collect user address

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import { useSelector, useDispatch } from "react-redux";
 import SigninScreen from "./screens/SigninScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import { signout } from "./actions/userAction";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
 
 function App() {
   const cart  = useSelector((state) => state.cart);
@@ -60,6 +61,7 @@ function App() {
           <Route path="/" component={HomeScreen} exact></Route>
           <Route path="/signin" component={SigninScreen}></Route>
           <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
         </main>
         <footer className="row center">All rights reserved</footer>
       </div>
--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -1,4 +1,4 @@
-import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "../constants/cartConstant";
+import { CART_ADD_ITEM, CART_REMOVE_ITEM, CART_SAVE_SHIPPING_ADDRESS } from "../constants/cartConstant";
 import Axios from "axios";
 
 export const addToCart = (productId, qty) => async(dispatch, getState) => {
@@ -25,4 +25,9 @@ export const removeFromCart = (productId) => (dispatch, getState) => {
   });
 
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
+
+export const saveShippingAddress = (data) => (dispatch) => {
+  dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
+  localStorage.setItem("shippingAddress", JSON.stringify(data));
 };

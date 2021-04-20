93031f6b6731fb743241a5995534b6d5e8649b07
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2021-04-20 15:34:01 +0100

    no message

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,22 @@ require("./db/mongoose");
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
+const path = require('path');
 const userRouter = require("./routers/userRouter");
 const productRouter = require("./routers/productRouter");
 const orderRouter = require("./routers/orderRouter");
 
 app.use(express.json());
+
+app.use(express.static("frontend/build"));
+
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+});
 // app.get("/", (req, res) => res.send("Server is ready"));
 
 app.listen(port, () => console.log(`Listening to server at port: ${port}`));

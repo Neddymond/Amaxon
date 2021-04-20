6b6c2e73a012f32d5171edc1709bfd37dfdc0c00
Chinedu codePanda Ikechi <Neddymond10@gmail.com>
2021-04-20 14:01:34 +0100

    Updates MongoDB configuration

--- backend/db/mongoose.js
+++ backend/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost/amaxon", {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true
